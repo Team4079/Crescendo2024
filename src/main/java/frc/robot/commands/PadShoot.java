334cd8f102e33835ed00f8c96ef6daa86f85513c
Malefic <ogupta4242@gmail.com>
2024-09-11 13:28:54 -0700

    PadPivot and PadShoot reformatting

--- src/main/java/frc/robot/commands/PadShoot.java
+++ src/main/java/frc/robot/commands/PadShoot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -10,15 +6,29 @@ import frc.robot.subsystems.Pivot;
 import frc.robot.subsystems.Shooter;
 import frc.robot.utils.LogitechGamingPad;
 
+/** The {@link PadShoot} class is a command that controls the shooter using a gamepad. */
 public class PadShoot extends Command {
-  /** Creates a new PadShoot. */
-  private Shooter shooter;
-  private LogitechGamingPad pad;
-  private Limelight limelight;
-  private Pivot pivot;
+  /** The Shooter subsystem used by this command. */
+  private final Shooter shooter;
 
+  /** The gamepad used to control the shooter. */
+  private final LogitechGamingPad pad;
+
+  /** The Limelight subsystem used by this command. */
+  private final Limelight limelight;
+
+  /** The Pivot subsystem used by this command. */
+  private final Pivot pivot;
+
+  /**
+   * Creates a new PadShoot command.
+   *
+   * @param shooter The Shooter subsystem used by this command.
+   * @param pad The gamepad used to control the shooter.
+   * @param limelight The Limelight subsystem used by this command.
+   * @param pivot The Pivot subsystem used by this command.
+   */
   public PadShoot(Shooter shooter, LogitechGamingPad pad, Limelight limelight, Pivot pivot) {
-    // Use addRequirements() here to declare subsystem dependencies.
     this.shooter = shooter;
     this.pad = pad;
     this.limelight = limelight;
@@ -26,15 +36,18 @@ public class PadShoot extends Command {
     addRequirements(shooter);
   }
 
-  // Called when the command is initially scheduled.
+  /** Called when the command is initially scheduled. */
   @Override
-  public void initialize() {}
+  public void initialize() {
+    // No specific action needed when the command is initialized.
+  }
 
-  // Called every time the scheduler runs while the command is scheduled.
+  /** Called every time the scheduler runs while the command is scheduled. */
   @Override
   public void execute() {
     // if (opPad.getRightBumperReleased()) {
-    //   shooter.setShooterVelocity(ShooterGlobalValues.PASSTHROUGH_RPS, ShooterGlobalValues.PASSTHROUGH_RPS);
+    //   shooter.setShooterVelocity(ShooterGlobalValues.PASSTHROUGH_RPS,
+    // ShooterGlobalValues.PASSTHROUGH_RPS);
     // } else {
     //   shooter.stopShooter();
     // }
@@ -46,22 +59,28 @@ public class PadShoot extends Command {
     }
 
     if (pad.getDPadRight()) {
-      new AmpScoreAlt(shooter, pivot, limelight).schedule();
+      new AmpScoreAlt(shooter, pivot, limelight).schedule(); // Uses Alt
+    } else {
+      shooter.stopShooter();
     }
-      else { 
-        shooter.stopShooter();
-      }
-    }
+  }
 
-  // Called once the command ends or is interrupted.
+  /**
+   * Called once the command ends or is interrupted.
+   *
+   * @param interrupted Whether the command was interrupted/canceled.
+   */
   @Override
   public void end(boolean interrupted) {
     shooter.stopKraken();
   }
 
-  // Returns true when the command should end.
+  /**
+   * Returns true when the command should end.
+   *
+   * @return false, as this command never finishes on its own.
+   */
   @Override
-
   public boolean isFinished() {
     return false;
   }
