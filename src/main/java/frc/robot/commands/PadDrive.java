a1440c73da1f62786d867d3ef9bac7354b6a1db1
Malefic <ogupta4242@gmail.com>
2024-09-11 13:24:10 -0700

    PadDrive, TeleOpAlign, and ManualShoot reformatting

--- src/main/java/frc/robot/commands/PadDrive.java
+++ src/main/java/frc/robot/commands/PadDrive.java
@@ -1,46 +1,104 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.utils.GlobalsValues.MotorGlobalValues;
 import frc.robot.utils.GlobalsValues.SwerveGlobalValues;
-import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.utils.LogitechGamingPad;
 
 /** The {@link PadDrive} command is the command that drives the robot using a gamepad. */
 public class PadDrive extends Command {
-
+  /** The SwerveSubsystem used by this command. */
   private final SwerveSubsystem swerveSubsystem;
+
+  /** Whether the drive is field-oriented. */
   private final boolean isFieldOriented;
+
+  /** The gamepad used to control the robot. */
   private final LogitechGamingPad pad;
 
-  /** Creates a new SwerveJoystick. */
-  public PadDrive(SwerveSubsystem swerveSubsystem,
-      LogitechGamingPad pad,
-      boolean isFieldOriented) {
+  /**
+   * Creates a new PadDrive command.
+   *
+   * @param swerveSubsystem The SwerveSubsystem used by this command.
+   * @param pad The gamepad used to control the robot.
+   * @param isFieldOriented Whether the drive is field-oriented.
+   */
+  public PadDrive(SwerveSubsystem swerveSubsystem, LogitechGamingPad pad, boolean isFieldOriented) {
     this.swerveSubsystem = swerveSubsystem;
     this.pad = pad;
     this.isFieldOriented = isFieldOriented;
-
-    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(this.swerveSubsystem);
   }
 
-  // Called when the command is initially scheduled.
+  /** Called when the command is initially scheduled. */
   @Override
-  public void initialize() {}
+  public void initialize() {
+    // No specific action needed when the command is initialized.
+  }
 
-  // Called every time the scheduler runs while the command is scheduled.
+  /** Called every time the scheduler runs while the command is scheduled. */
   @Override
   public void execute() {
-    double y;
-    double x;
-    double rotation;
+    Coordinate position = positionSet(pad);
+    double rotation = pad.getRightAnalogXAxis();
 
+    double turn = 0;
+
+    if (Math.abs(rotation) < 0.05) {
+      rotation = 0;
+    }
+
+    SmartDashboard.putNumber("X axis joystick", position.getX());
+    SmartDashboard.putNumber("y axis joystick", position.getY());
+
+    turn = rotation * MotorGlobalValues.MAX_ANGULAR_SPEED * 2 * 1.5; // magic numbers
+
+    if (MotorGlobalValues.AACORN_MODE) {
+      swerveSubsystem.drive(
+          -position.getX() * MotorGlobalValues.AACORN_SPEED,
+          position.getY() * MotorGlobalValues.AACORN_SPEED,
+          turn,
+          isFieldOriented);
+    } else {
+      swerveSubsystem.drive(
+          -position.getX() * MotorGlobalValues.SPEED_CONSTANT,
+          position.getY() * MotorGlobalValues.SPEED_CONSTANT,
+          turn,
+          isFieldOriented);
+    }
+  }
+
+  /**
+   * Called once the command ends or is interrupted.
+   *
+   * @param interrupted Whether the command was interrupted/canceled.
+   */
+  @Override
+  public void end(boolean interrupted) {
+    // No specific action needed when the command ends.
+  }
+
+  /**
+   * Returns true when the command should end.
+   *
+   * @return false, as this command never finishes on its own.
+   */
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  /**
+   * Sets the position based on the gamepad input.
+   *
+   * @param pad The gamepad used to control the robot.
+   * @return The coordinate representing the position.
+   */
+  public static Coordinate positionSet(LogitechGamingPad pad) {
+    double x;
+    double y;
     if (MotorGlobalValues.SLOW_MODE) {
       y = pad.getLeftAnalogXAxis() * MotorGlobalValues.MAX_SPEED * MotorGlobalValues.SLOW_SPEED;
       x = pad.getLeftAnalogYAxis() * -MotorGlobalValues.MAX_SPEED * MotorGlobalValues.SLOW_SPEED;
@@ -59,55 +117,27 @@ public class PadDrive extends Command {
     if (Math.abs(pad.getLeftAnalogYAxis()) < SwerveGlobalValues.JOYSTICK_DEADBAND) {
       x = 0;
     }
-
-    rotation = pad.getRightAnalogXAxis();
-
-    double turn = 0;
-    double heading_deadband = 0.2;
-    double controller_deadband = 0.05;
-
-    // // If the right joystick is within the deadband, don't turn
-    // if (Math.abs(pad.getRightAnalogXAxis()) <= controller_deadband) {
-    //   if (MotorGlobalValues.HEADING > (swerveSubsystem.pgetHeading() + heading_deadband)) {
-    //     turn = -MotorGlobalValues.MAX_ANGULAR_SPEED;
-    //   } else if (MotorGlobalValues.HEADING < (swerveSubsystem.pgetHeading() - heading_deadband)) {
-    //     turn = MotorGlobalValues.MAX_ANGULAR_SPEED;
-    //   } else {
-    //     turn = 0;
-    //   } 
-    // } else {
-    //   turn = pad.getRightAnalogXAxis() * MotorGlobalValues.MAX_ANGULAR_SPEED;
-    //   MotorGlobalValues.HEADING = swerveSubsystem.pgetHeading();
-    // }
-
-    if (Math.abs(rotation) < 0.05) {
-      rotation = 0;
-    }
-
-    SmartDashboard.putNumber("X axis joystick", x);
-    SmartDashboard.putNumber("y axis joystick", y);
-
-    turn = rotation * MotorGlobalValues.MAX_ANGULAR_SPEED * 2 * 1.5;
-
-    // x *= -1;
-    // y *= -1;
-    // turn *= -1;
-
-    if (MotorGlobalValues.AACORN_MODE) {
-      swerveSubsystem.drive(-x * MotorGlobalValues.AACORN_SPEED, y * MotorGlobalValues.AACORN_SPEED, turn, isFieldOriented);
-    } else {
-      swerveSubsystem.drive(-x * MotorGlobalValues.SPEED_CONSTANT, y * MotorGlobalValues.SPEED_CONSTANT,
-          turn, isFieldOriented);
-    }
+    return new Coordinate(x, y);
   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+  /** A record representing a coordinate with x and y values. */
+  public record Coordinate(double x, double y) {
+    /**
+     * Gets the x value of the coordinate.
+     *
+     * @return The x value.
+     */
+    public double getX() {
+      return x;
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+    /**
+     * Gets the y value of the coordinate.
+     *
+     * @return The y value.
+     */
+    public double getY() {
+      return y;
+    }
   }
 }
