ba5326bd325ada639dca240ed7d9c79483d888d9
Om Gupta <ogupta4242@gmail.com>
2024-10-14 18:58:52 -0700

    [bug] Changed Jayden's equation He can't type in math equations correctly fsr Or maybe I'm just trolling

--- src/main/java/frc/robot/subsystems/Photonvision.java
+++ src/main/java/frc/robot/subsystems/Photonvision.java
@@ -9,11 +9,8 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.GlobalsValues.PhotonVisionConstants;
-
 import java.util.List;
 import java.util.Optional;
-
-import org.opencv.photo.Photo;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
@@ -72,7 +69,10 @@ public class Photonvision extends SubsystemBase {
   public Photonvision() {
     photonPoseEstimatorleft =
         new PhotonPoseEstimator(
-            aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, cameraleft, leftCameraPos);
+            aprilTagFieldLayout,
+            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+            cameraleft,
+            leftCameraPos);
     photonPoseEstimatorright =
         new PhotonPoseEstimator(
             aprilTagFieldLayout,
@@ -93,7 +93,7 @@ public class Photonvision extends SubsystemBase {
     double distleft = 0;
     double distright = 0;
 
-    if (resultleft.hasTargets()){
+    if (resultleft.hasTargets()) {
       targetleft = resultleft.getBestTarget();
       targetPoseAmbiguityleft = targetleft.getPoseAmbiguity();
 
@@ -104,21 +104,17 @@ public class Photonvision extends SubsystemBase {
       //   Transform3d fieldToCamera = resultleft.getMultiTagResult().estimatedPose.best;
       //   SmartDashboard.putNumber("field to camera", fieldToCamera.getX());
       // }
-    } 
-
-    else {
+    } else {
       targetPoseAmbiguityleft = 7157;
     }
 
-    if (resultright.hasTargets()){
+    if (resultright.hasTargets()) {
       targetright = resultright.getBestTarget();
       targetPoseAmbiguityright = targetright.getPoseAmbiguity();
 
       distright = targetright.getBestCameraToTarget().getTranslation().getNorm();
       SmartDashboard.putNumber("distright", distright);
-    } 
-
-    else{
+    } else {
       targetPoseAmbiguityright = 7157;
     }
 
@@ -129,7 +125,6 @@ public class Photonvision extends SubsystemBase {
     SmartDashboard.putNumber("right cam ambiguity", targetPoseAmbiguityright);
     SmartDashboard.putBoolean("right_targets", resultright.hasTargets());
     SmartDashboard.putBoolean("left_targets", resultleft.hasTargets());
-
   }
 
   /**
@@ -222,13 +217,11 @@ public class Photonvision extends SubsystemBase {
   }
 
   public double getOffset(PhotonCamera camera) {
-    if (camera.getName().equals("Left"))
-    {
+    if (camera.getName().equals("Left")) {
       return PhotonVisionConstants.OFFSET_TOWARD_MID_LEFT;
     }
 
-    if (camera.getName().equals("Right"))
-    {
+    if (camera.getName().equals("Right")) {
       return PhotonVisionConstants.OFFSET_TOWARD_MID_RIGHT;
     }
 
@@ -236,16 +229,22 @@ public class Photonvision extends SubsystemBase {
   }
 
   public double getPivotPosition() {
-    // return (-0.273166 * Math.pow(getRange(), 5)
-    //     + 4.16168 * Math.pow(getRange(), 4)
-    //     + -23.6466 * Math.pow(getRange(), 3)
-    //     + 60.022 * Math.pow(getRange(), 2)
-    //     + getRange() * -58.4714
-    //     + 27.1329);
-
     // 10/14/2024 outside tuning
-
-    return ((-0.044599 * Math.pow(getRange(), 4)) + (1.41778 * Math.pow(getRange(), 2))  + (12.3919 * getRange()) + (-5));
+    // jayden why are you so bad at tuning
+    // Desmos: https://www.desmos.com/calculator/naalukjxze
+    double r = getRange();
+    double f = -1.19541; // power 5
+    double e = 18.0904; // power 4
+    double d = -108.07; // power 3
+    double c = 317.396; // power 2
+    double b = -453.088; // power 1
+    double a = 267.288; // constant
+    return ((f * Math.pow(r, 5))
+        + (e * Math.pow(r, 4))
+        + (d * Math.pow(r, 3))
+        + (c * Math.pow(r, 2))
+        + (b * r)
+        + a);
   }
 
   public Translation3d conv2dTo3d(Translation2d translation2d, double z) {
@@ -255,7 +254,7 @@ public class Photonvision extends SubsystemBase {
   public PhotonCamera getBestCamera() {
     return (targetPoseAmbiguityleft > targetPoseAmbiguityright) ? cameraright : cameraleft;
   }
-  
+
   public double getYaw(PhotonCamera camera) {
     List<PhotonTrackedTarget> results = camera.getLatestResult().getTargets();
     boolean frontOfSubwoofer = false;
@@ -268,8 +267,7 @@ public class Photonvision extends SubsystemBase {
 
     SmartDashboard.putBoolean("front of subwoofer", frontOfSubwoofer);
 
-    if (camera.getLatestResult().hasTargets() && frontOfSubwoofer)
-    {
+    if (camera.getLatestResult().hasTargets() && frontOfSubwoofer) {
       return camera.getLatestResult().getBestTarget().getYaw();
     }
     return 4079;
