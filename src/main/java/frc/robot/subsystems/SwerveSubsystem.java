60cb5bfb6d6348390ab629e64ec9347b7b093ab3
CalamityCow <jaydenisgud@gmail.com>
2024-10-22 16:44:58 -0700

    Removed unused imports
    Co-authored-by: Jayden Sun <jaydenisgud@gmail.com>
    Co-authored-by: Shawn Gazin <shawngazin@protonmail.com>
    Co-authored-by: Erick Tran <thelittlebotengineer@gmail.com>
    Co-authored-by: Brandon Jun <arachnite.b@gmail.com>
    Co-authored-by: Om Gupta <om@malefic.xyz>
    Co-authored-by: Theodore Vang <theodorevangoa26@gmail.com>
    Co-authored-by: Aaron Truong <aaronpt30@gmail.com>
    Co-authored-by: Ria Mishra <Mishra.r1309@gmail.com>
    Co-authored-by: Serenity Zheng <serenityzheng@gmail.com>

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.util.DriveFeedforwards;
@@ -16,7 +15,6 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
